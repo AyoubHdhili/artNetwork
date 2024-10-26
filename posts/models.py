0f9220d77e769eb9ad7c2d68994293564a8bf90a
mohamedgabsi <mohamedlaroussi.gabsi@esprit.tn>
2024-10-26 22:41:52 +0100

    crud post

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
+    title = models.CharField(max_length=200, blank=False)
+    content = models.TextField(blank=False)
+    image = models.ImageField(upload_to='post_images/', blank=False, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404, redirect
+from .models import Post
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# Adding index view for completeness
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'posts/index.html')
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'posts/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'posts/post_detail.html', {'post': post})
+
+@login_required
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user  # Set the author to the logged-in user
+            post.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+    return render(request, 'posts/post_form.html', {'form': form})
+
+@login_required
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.user != post.author:  # Check if the user is the author
+        return redirect('post_list')  # Redirect if not the author
+    if request.method == 'POST':
+        post.delete()
+        return redirect('post_list')
+    return render(request, 'posts/post_confirm_delete.html', {'post': post})
+
+def post_update(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post_list')  
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'posts/post_update.html', {'post': post, 'form': form})
+
+@login_required
+def my_posts(request):
+    posts = Post.objects.filter(author=request.user).order_by('-created_at')
+    return render(request, 'posts/myPosts.html', {'posts': posts})
