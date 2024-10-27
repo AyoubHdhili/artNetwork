f0c886b5cad12c4766d627a36267915394b92e50
fad-anes <anes.fad@esprit.tn>
2024-10-27 20:15:05 +0100

    add backoffice

--- Participation/admin.py
+++ Participation/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Event,Participation
 
-# Register your models here.
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start_date', 'max_participants', 'creator')
+    list_filter = ('start_date', 'max_participants')
+    search_fields = ('title', 'description')
+
+@admin.register(Participation)
+class ParticipationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'event', 'participation_date')
+    list_filter = ('participation_date',)
+    search_fields = ('user__username', 'event__title')
